a551837f319c2b9e95a3cd01ccf2c68828811309
timothee <timbarattin@gmail.com>
2026-02-18 13:31:26 +0100

    [UPDATE] Improve print version

--- portfolio.js
+++ portfolio.js
@@ -2,14 +2,14 @@ window.addEventListener('resize', function (e) {
   windowWidthMediaQuery = window.matchMedia('(min-width: 650px)');
   if (windowWidthMediaQuery.matches) {
     toggleDetailsElements(detailsEl, true);
-  } else if (!windowWidthMediaQuery.matches) {
+  } else {
     toggleDetailsElements(detailsEl, false);
   }
 });
 
 function toggleMore(buttonEl, section) {
   const moreEl = document.getElementsByClassName('details__more ' + section);
-  for (var i = 0; i < moreEl.length; i++) {
+  for (let i = 0; i < moreEl.length; i++) {
     if (moreEl.item(i).style.display === 'none') {
       moreEl.item(i).style.display = 'block';
       buttonEl.innerHTML = 'See Less';
